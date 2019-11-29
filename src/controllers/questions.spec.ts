0ae2e44ab11d570c44aadcdb6835d1a3507a0cad
Acinate <Acinate@hotmail.com>
2019-11-29 01:18:32 -0500

    improved test cases

--- src/controllers/questions.spec.ts
+++ src/controllers/questions.spec.ts
@@ -16,35 +16,50 @@ const question: Question = {
     incorrect_1: 'test_incorrect_1',
     incorrect_2: 'test_incorrect_2',
     incorrect_3: 'test_incorrect3',
-    category_id: 1,
+    deleted: 0,
     category_name: '',
-    deleted: 0
+    category_id: 1
 };
 
-it('Test questions endpoint', async done => {
+it('Posts a question', async done => {
     // Send POST Request to /questions
-    let response: Response = await request.post('/questions').send(question);
+    const response: Response = await request.post('/questions').send(question);
     expect(response.status).toBe(200);
     question.question_id = response.body.insertId;
+    done();
+});
 
+it('Gets questions', async done => {
     // Sends GET Request to /questions
-    response = await request.get('/questions?page=1&deleted=false');
+    const response: Response = await request.get('/questions?page=1&deleted=false');
     expect(response.status).toBe(200);
+    done();
+});
 
-    // // Sends GET Request to /questions
-    // response = await request.get('/questions?page=1&deleted=false&category=calculus');
-    // expect(response.status).toBe(200);
-
+it('Gets questions by Category Name', async done => {
     // Sends GET Request to /questions
-    response = await request.get(`/questions?page=1&deleted=false&id=${question.question_id}`);
+    const response: Response = await request.get('/questions?page=1&deleted=false&category=calculus');
     expect(response.status).toBe(200);
+    done();
+});
 
+it('Gets questions by Question Id', async done => {
+    // Sends GET Request to /questions
+    const response: Response = await request.get(`/questions?page=1&deleted=false&id=${question.question_id}`);
+    expect(response.status).toBe(200);
+    done();
+});
+
+it('Updates a question', async done => {
     // SEND PUT Request to /questions
-    response = await request.put('/questions').send(question);
+    const response: Response = await request.put('/questions').send(question);
     expect(response.status).toBe(200);
+    done();
+});
 
+it('Deletes a question by Question Id', async done => {
     // Send DELETE Request to /questions?questionId={id}
-    response = await request.delete(`/questions?id=${question.question_id}`);
+    const response: Response = await request.delete(`/questions?id=${question.question_id}`);
     expect(response.status).toBe(200);
     done();
 });
