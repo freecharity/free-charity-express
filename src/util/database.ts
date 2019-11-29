cca38c680f5d159e3dda99123ecc795ac4ab41ce
Acinate <Acinate@hotmail.com>
2019-11-29 13:59:52 -0500

    database now uses connection pooling, up to 50 connections allowed

--- src/util/database.ts
+++ src/util/database.ts
@@ -1,11 +1,10 @@
-import mysql from "mysql";
-import {DATABASE} from "./secrets";
+import mysql from 'mysql';
+import {DATABASE} from './secrets';
 
-export const connection = mysql.createConnection({
+export const connection = mysql.createPool({
+    connectionLimit: 50,
     host: DATABASE.host,
     user: DATABASE.user,
     password: DATABASE.password,
     database: DATABASE.database
 });
-
-connection.connect();
